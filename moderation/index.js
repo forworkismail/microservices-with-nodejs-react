809d725e909a5ad9267f3b1d195b96ee5b7dd475
Ismail <forwork.ismail@gmail.com>
2022-02-18 23:52:03 +0430

    Create CommentModerated event in moderation service

--- moderation/index.js
+++ moderation/index.js
@@ -5,6 +5,25 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: post.content
+      }
+    })
+  }
+
+  res.send({});
+});
+
 app.listen(4003, () => {
   console.log('listening on port 4003');
 });
