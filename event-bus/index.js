427559ab2ed66e5d31095116b776f4898c243dd1
Ismail <forwork.ismail@gmail.com>
2022-02-19 22:52:21 +0430

    Create get events handler in event bus service to fetch events

--- event-bus/index.js
+++ event-bus/index.js
@@ -20,6 +20,10 @@ app.post('/events', (req, res) => {
   res.send({ status: 'OK' });
 });
 
+app.get('/events', (req, res) => {
+  res.send(events);
+})
+
 app.listen(4005, () => {
   console.log('listening on port 4005')
 });
